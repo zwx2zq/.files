da2415f4342957b7a304a6006d9481eb93415316
Elve <elve2002@live.se>
2026-06-19 11:56:43 +0200

    sway bar battery level

--- sway/.config/sway/bar.sh
+++ sway/.config/sway/bar.sh
@@ -6,4 +6,7 @@ VOLUME=$(pactl get-sink-volume \@DEFAULT_SINK@ | head -n1 | awk '{print $5}')
 
 BRIGHTNESS_VALUE=$(brightnessctl get)
 
-echo "$(($BRIGHTNESS_VALUE / 5))%" "|" $VOLUME "|" $DATE
+BATTERY_LEVEL=$(upower -i /org/freedesktop/UPower/devices/battery_macsmc_battery | grep -e percentage | awk '{print $2}')
+BATTERY_STATE=$(upower -i /org/freedesktop/UPower/devices/battery_macsmc_battery | grep -e state | awk '{print $2}')
+
+echo B: "$(($BRIGHTNESS_VALUE / 5))%" "|" V: $VOLUME "|" C: $BATTERY_LEVEL "($BATTERY_STATE)" "|" $DATE
